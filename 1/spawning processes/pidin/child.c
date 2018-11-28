bb570ddc20189f60d7bcf5d1e33003ba73cbc3a3
o0white0o <aldrozdovskij@gmail.com>
2018-11-28 06:51:20 +0300

    Init commit

--- 1/spawning processes/pidin/child.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(int argc,char *argv[]){
-  printf("Process %s started, pid %d, ppid %d\n",argv[1],getpid(),getppid());
-  int count=0;
-  sleep(3);
-  return 0;
-}
--- 1/spawning processes/spawn/child.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(int argc,char *argv[]){
-  printf("Process %s started, pid %d, ppid %d\n",argv[1],getpid(),getppid());
-  return 0;
-}
